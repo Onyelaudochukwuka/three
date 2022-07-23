c538c21be3774c457c219959a7477bd5ed006bcc
Udoka <udochukwukaonyela@gmail.com>
2022-07-23 19:00:20 +0100

    created shadows

--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -22,21 +22,22 @@ const Three = () => {
           <OrbitControls ref={OrbitControlsEl} minPolarAngle={angleToRadians(40)} maxPolarAngle={angleToRadians(80)} />
           
           {/* Ball */}
-          <mesh position={[0,0.5,0]}>
+          <mesh position={[0,0.5,0]} castShadow>
               <sphereGeometry args={[0.5, 32, 32]} />
               <meshStandardMaterial color="#fff"/>
           </mesh>
 
           {/* Floor */}
-          <mesh rotation={[-(angleToRadians(90)), 0, 0]} >
+          <mesh rotation={[-(angleToRadians(90)), 0, 0]} receiveShadow>
               <planeGeometry args={[7, 7]} />
               <meshPhongMaterial color="#1ea3d8"/>
           </mesh>
 
           {/* Ambient Light */}
           <ambientLight args={["#ffffff", 0.25]} />
-          {/* Directional Light */}
-          <spotLight args={["#ffffff", 1,2]} position={[-3, 1, 0]} />
+
+          {/* SpotLight */}
+          <spotLight args={["#ffffff", 1.5,7,angleToRadians(45),0.4]} position={[-3, 1, 0]} castShadow/>
       </>
   )
 }
