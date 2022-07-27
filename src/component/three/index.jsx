8c00997fcc84beed59771fb7bacd67d1e73ccde5
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 14:11:31 +0100

    changes

--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls, PerspectiveCamera, useTexture } from "@react-three/drei";
+import { Environment, OrbitControls, PerspectiveCamera, Html } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import { angleToRadians } from "../../utils/angle";
@@ -7,35 +7,35 @@ import Car from './car';
 import Ball from "./ball";
 const Three = () => {
     const [position, setPosition] = useState([3, 0.65, 0]);
-    const [array, setArray] = useState([1])
+    const [array, setArray] = useState([1]);
+    const [score, setScore] = useState(0)
 
     const OrbitControlsEl = useRef();
     useFrame((state) => {
             const { x, y } = state.mouse;
             setPosition([-x * 10, 0.65, y * 10]);
     },[])
-useEffect(() => {
-  
-
-
-}, [position])
     useEffect(() => {
         const interval = setInterval(() => {
-            setArray((prev)=>[...prev,1])
+            setArray((prev) => [...prev, 1]);
+            setScore((prev) => prev + 10);
         }, 5000);
         return () => clearInterval(interval);
     }, []);
-    // Animation
+
 
   return (
       <>
           <PerspectiveCamera makeDefault position={[0, 15, -15]} />
           <OrbitControls ref={OrbitControlsEl} minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(90)} />
-          
+          <Html>
+              <div style={{ fontSize: 50, fontWeight: 600, zIndex: -1, position: 'absolute', top: '50%', left:'50%' }}>{score}</div>
+          </Html>
+
           {/* Ball */}
           <group>
           {array.map((a,i) =>
-              <Ball key={i} pow={i} />
+              <Ball key={i} pow={i} position={position} />
               )}
           </group>
           {/* Car */}
