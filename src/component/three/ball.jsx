439e56a9f454575d5b7d6431999b18a986ea58ac
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 12:26:38 +0100

    fixed animation

--- src/component/three/ball.jsx
+++ src/component/three/ball.jsx
@@ -10,9 +10,9 @@ const Ball = (props) => {
             const timeline = gsap.timeline();
             // x-axis motion
             timeline.to(ballRef.current.position, {
-                x: 1,
-                duration: 2,
-                ease: "power2.in"
+                z: -60,
+                duration: 7.5 * Math.pow(0.99,props.pow),
+                ease: "power2.out"
             });
 
             // y-axis motion
@@ -29,7 +29,7 @@ const Ball = (props) => {
         }
     }, [ballRef.current])
   return (
-      <mesh position={[-2, 2.5, 0]} {...props} castShadow ref={ballRef}>
+      <mesh position={[0, 1, 60]} {...props} scale={4} castShadow ref={ballRef}>
           <sphereGeometry args={[0.5, 32, 32]} />
           <meshStandardMaterial color="#ffffff" metalness={0.75} roughness={0.3} />
       </mesh>
