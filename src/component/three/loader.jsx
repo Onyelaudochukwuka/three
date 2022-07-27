4b609977787b4be4d66843084de669bd642cf563
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 14:29:25 +0100

    fixes

--- src/component/three/loader.jsx
+++ src/component/three/loader.jsx
@@ -3,5 +3,5 @@ import { Html, useProgress } from '@react-three/drei'
 export function Loader() {
     const { progress } = useProgress();
     console.log(progress)
-    return <Html center style={{ backgroundColor: "#fff", display: 'flex', flex: 1, justifyContent: 'center', alignItems:'center', minHeight: '100vh', minWidth: '100vw'}}>{progress} % loaded</Html>
+    return <Html center style={{ backgroundColor: "#fff", display: 'flex', flex: 1, justifyContent: 'center', alignItems:'center', minHeight: '100vh', minWidth: '100vw', fontWeight:800}}>{progress} % loaded</Html>
 }
