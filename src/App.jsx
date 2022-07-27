a8c09012a286b94422215bad4fdaaabff3db5e9b
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 12:13:28 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Canvas } from '@react-three/fiber'
 import { Suspense } from "react";
 import Three from './component/three'
 import './App.css'
+import { Loader } from "./component/three/loader";
 
 function App() {
   return (
     <Canvas id="three-canvas-container" shadows>
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader />}>
         <Three />
         </Suspense>
     </Canvas>
