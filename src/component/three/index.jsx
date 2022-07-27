b612fd79d9e048387ca9394cad3dc4349778e3d2
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 12:13:34 +0100

    logic for game

--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -3,18 +3,21 @@ import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import { angleToRadians } from "../../utils/angle";
 import * as THREE from "three";
-import gsap from "gsap";
 import Car from './car';
+import Ball from "./ball";
 const Three = () => {
-    const [position, setPosition] = useState([3, 0.65, 0])
+    const [position, setPosition] = useState([3, 0.65, 0]);
+    const [array, setArray] = useState([1])
+    setInterval(() => {
+        setArray((prev) => {
+            return [...array, 1]
+        })
+    }, 10000);
     const OrbitControlsEl = useRef();
     useFrame((state) => {
-        if (OrbitControlsEl.current) {
             const { x, y } = state.mouse;
             setPosition([-x * 10, 0.65, y * 10]);
-            console.log(position)
-        }
-    })
+    },[])
 useEffect(() => {
   
 
@@ -23,47 +26,19 @@ useEffect(() => {
 
     // Animation
 
-    const ballRef = useRef();
-    useEffect(() => {
-        if (ballRef.current) {
-
-            //timeline
-            const timeline = gsap.timeline();
-            // x-axis motion
-            timeline.to(ballRef.current.position, {
-                x: 1,
-                duration: 2,
-                ease: "power2.in"
-            });
-
-            // y-axis motion
-            timeline.to(ballRef.current.position, {
-                y: 0.5,
-                duration: 1,
-                ease: "bounce.out"
-            }, "<"),
-                
-            
-            
-            // Play
-            timeline.play()
-        } 
-    },[ballRef.current])
   return (
       <>
-          <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-          <OrbitControls ref={OrbitControlsEl} minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(80)} />
+          <PerspectiveCamera makeDefault position={[0, 10, -10]} />
+          <OrbitControls ref={OrbitControlsEl} minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(90)} />
           
           {/* Ball */}
-          <mesh position={[-2,2.5,0]} castShadow ref={ballRef}>
-              <sphereGeometry args={[0.5, 32, 32]} />
-              <meshStandardMaterial color="#ffffff" metalness={0.75} roughness={0.3} />
-          </mesh>
-
+          {array.map((a,i) =>
+              <Ball key={i}/>
+          )}
           {/* Car */}
-          <Car position={position} castShadow/>
+          {/* <Car position={position} castShadow/> */}
           {/* Floor */}
-          <mesh rotation={[-(angleToRadians(90)), 0, 0]} receiveShadow>
+          <mesh rotation={[-(angleToRadians(90)), 0, 0]} scale={100} receiveShadow>
               <planeGeometry args={[20, 20]} />
               <meshStandardMaterial color="#1ea3d8"/>
           </mesh>
@@ -72,7 +47,7 @@ useEffect(() => {
           <ambientLight args={["#ffffff", 0.35]} />
 
           {/* SpotLight */}
-          <spotLight args={["#ffffff", 1.5, 15, angleToRadians(45), 0.4]} position={[-3, 1, 0]} castShadow />
+          <spotLight args={["#ffffff", 3.5, 1590, angleToRadians(45), 0.4]} position={[0, 1, 60]} castShadow />
           
           {/* Environment */}
           <Environment background>
